19cee159334622c2e9684935227d4b8255db46e6
Elti Shehu <shehuelti@gmail.com>
2018-09-27 09:36:36 +0200

    Added java configuration example.

---
+++ src/main/java/com/elti/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package com.elti.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by e.sh. on 27-Sep-18
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/com/elti/joke/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/com/elti/joke/jokeapp/services/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.elti.joke.jokeapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,8 +12,8 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
